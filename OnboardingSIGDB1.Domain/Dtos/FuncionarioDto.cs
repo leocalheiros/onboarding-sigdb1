2fbfd00adaf4873543e322d654c7e32bf81c5dad
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2024-04-19 17:56:33 -0300

    add dto

---
+++ OnboardingSIGDB1.Domain/Dtos/FuncionarioDto.cs
@@ -0,0 +1,19 @@
+﻿namespace OnboardingSIGDB1.Domain.Dtos;
+
+
+public class FuncionarioDto
+{
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public CargosDto Cargos { get; set; }
+    public string EmpresaNome { get; set; }
+    public string Cpf { get; set; }
+    public DateTimeOffset DataContratacao { get; set; }
+}
+
+public class CargosDto
+{
+    public long Id { get; set; }
+    public string CargoDescricao { get; set; }
+    
+}
