bf130df50a70c63a1e26ca3bf66f0c66d3d7be9b
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2024-04-19 17:56:53 -0300

    modificações faltando para bugfix

--- OnboardingSIGDB1.Domain/Interfaces/IFuncionarioRepository.cs
+++ OnboardingSIGDB1.Domain/Interfaces/IFuncionarioRepository.cs
@@ -1,11 +1,12 @@
-﻿using OnboardingSIGDB1.Domain.Entities;
+﻿using OnboardingSIGDB1.Domain.Dtos;
+using OnboardingSIGDB1.Domain.Entities;
 using OnboardingSIGDB1.Domain.Entities.Common;
 
 namespace OnboardingSIGDB1.Domain.Interfaces;
 
 public interface IFuncionarioRepository : IBaseRepository<Funcionario>
 {
-    new Task<List<Funcionario>> GetAllAsync(CancellationToken cancellationToken);
+    new Task<List<FuncionarioDto>> GetAllAsync(CancellationToken cancellationToken);
     Task<Funcionario> FindByCpfAsync(string cpf, CancellationToken cancellationToken);
     Task<bool> FuncionarioHasEmpresaAsync(long funcionarioId, CancellationToken cancellationToken);
     Task<List<Funcionario>> GetEmpresasComFiltros(FuncionarioFiltro filtro, CancellationToken cancellationToken);
